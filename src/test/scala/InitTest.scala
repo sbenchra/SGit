f24b85574be7c1e13cb3b27b24f39a4f89f80888
sbenchra <soufianebenchraa2@gmail.com>
2019-10-17 23:38:40 +0200

    started Init test

--- src/test/scala/InitTest.scala
+++ src/test/scala/InitTest.scala
@@ -1,9 +1,28 @@
 import org.scalatest._
+import java.io.File
+import sgit.commands._
+import sgit.utilities._
 import sgit._
 
 class InitTest extends FunSuite with DiagrammedAssertions {
+  var dir = new File(System.getProperty("user.dir"))
+  var repo = Repository
 
-  test("First test") {
-    assert(1 == 1)
+  test("Repository shouldn't initialized") {
+    assert(dir.listFiles().map(_.getName()).contains(".sgit") == false)
   }
+  /*
+  var init = Init
+  init.Init()
+
+  Thread.sleep(9000)
+
+  dir = new File(System.getProperty("user.dir"))
+
+  test("Repository is correctly initialized") {
+    assert(dir.listFiles().map(_.getName()).contains(".sgit"))
+  }
+
+  FilesUtilities.deleteRecursively(new File("./.sgit"))
+  */
 }
