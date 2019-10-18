ae7848863cdbe482227c44ba42cbcc639ebd18de
Benchraa <soufianebenchraa2@gmail.com>
2019-10-19 01:35:06 +0200

    fixing test

--- src/test/scala/IndexTest.scala
+++ src/test/scala/IndexTest.scala
@@ -8,7 +8,7 @@ class IndexTest extends FunSuite with DiagrammedAssertions {
   val file=new File("soufiane/test.txt")
   test("Should test if it correctly creates index of a directory"){
 
-    assert(Index.workingDirIndex(List(file)).head==IndexEntry("/home/benchraa/SGit/soufiane/test.txt","7fb619752a1dc9aaf830ea4bbce341102e66028b")
+    assert(Index.workingDirIndex(List(file)).head==IndexEntry("/home/benchraa/SGit/soufiane/test.txt","29b40318f1b2a1c8b8a59dde07539ae6a05b9156")
     )
   }
 
